2862fbe6ef8074b7077133da0156b9d96ab7e0b8
mamo <rr0061vv@ed.ritsumei.ac.jp>
2017-05-11 21:15:56 +0900

    ラベル追従

--- BrainStorming/MapViewController.swift
+++ BrainStorming/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MapViewController.swift
+//  ViewController.swift
 //  BrainStorming
 //
 //  Created by 上田　護 on 2017/04/23.
@@ -11,6 +11,8 @@ import AVFoundation
 
 var preX: Double = 0
 var preY: Double = 0
+var oldX: Double = 0
+var oldY: Double = 0
 
 var newX: Double = 0
 var newY: Double = 0
@@ -20,6 +22,7 @@ var i: Float = 0.0
 var j: Float = 0.0
 var frame: UIView!
 let layer = CAShapeLayer()
+var newFrameLimit: Double = 0
 
 
 let pi = CGFloat(M_PI)
@@ -28,34 +31,33 @@ let end :CGFloat = pi * 2 // 終了の角度
 var path: UIBezierPath = UIBezierPath();
 
 var screenCenter: Int!
+var frameAmount: Int = 0
 
 class MapViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate{
     
     var myLabel: UILabel!
     //テキストボックスフィールドの変数
     private var myTextField: UITextField!
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //選択したレイヤーをいれておく
     private var selectLayer:CALayer!
-    
     //最後にタッチされた座標をいれておく
     private var touchLastPoint:CGPoint!
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    let ovalBtn = UIButton()
     
-    
-    
-    //--------------------------------------
+    weak var timer: Timer!
+    var startTime = Date()
     
     
     @IBOutlet weak var label: UILabel!
-    @IBAction func tap(sender: AnyObject) {
+    
+    func tap(/*sender: AnyObject*/) {
         
         let alert = UIAlertController(title: "文字を入力", message: "最初の文字を入力してください", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "完了", style: .default) { (action:UIAlertAction!) -> Void in
             
             // 入力したテキストをコンソールに表示
             let textField = alert.textFields![0] as UITextField
-            self.label.text = textField.text
+            self.myLabel.text = textField.text
         }
         
         let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (action:UIAlertAction!) -> Void in
@@ -70,72 +72,53 @@ class MapViewController: UIViewController, UITextFieldDelegate, UIGestureRecogni
         
         present(alert, animated: true, completion: nil)
     }
-
-    //---------------------------------------
-    
-//    var audioPlayer : AVAudioPlayer!
     var playerView: UIView!
     var firstView: UIView!
-   
+    
     
     //設定メソッド
     override func viewDidLoad() {
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Screen Size の取得
+        let screenWidth = self.view.bounds.width
+        let screenHeight = self.view.bounds.height
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         let width = self.view.bounds.width
         let height = self.view.bounds.height
-
-        //丸を生成するボタン
-        let ovalBtn = UIButton()
-        ovalBtn.frame = CGRect(x:0,y:0,width:100,height:50)
-        ovalBtn.center = CGPoint(x:width / 3,y:height - 30)
-        ovalBtn.addTarget(self, action: #selector(MapViewController.ovalBtnTapped(sender:)), for: .touchUpInside)
-        ovalBtn.setTitle("丸",for:.normal)
-//        ovalBtn.backgroundColor = UIColor.green
-        self.view.addSubview(ovalBtn)
         
+        
+        ovalBtn.frame = CGRect(x:0,y:0,width:40,height:40)        //丸を生成するボタン
+        ovalBtn.addTarget(self, action: #selector(MapViewController.ovalBtnTapped(sender:)), for: .touchUpInside)              //アクションを設定
+        ovalBtn.setTitle("丸",for:.normal)
+
         //丸を描く
         let oval = MyShapeLayer()
-        oval.frame = CGRect(x:30,y:30,width:80,height:80)
+        oval.frame = CGRect(x:screenWidth/2 - 40,y:screenHeight/2 - 40,width:80,height:80)
         oval.drawOval(lineWidth:1)
         self.view.layer.addSublayer(oval)
-        
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         
         super.viewDidLoad()
         
         //背景色の設定
         self.view.backgroundColor = UIColor.cyan
         
-        // Screen Size の取得
-        let screenWidth = self.view.bounds.width
-        let screenHeight = self.view.bounds.height
-        
-        //四角フレーム生成
-        firstView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-        firstView.backgroundColor = UIColor.red
-        self.firstView.center = CGPoint(x: screenWidth / 2, y: screenHeight / 2)
     }
-    
-    
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-    /************** Button Tapped ***********/
+
+    //ボタンがタップされた時
     func ovalBtnTapped(sender:UIButton){
         //丸を描く
         let oval = MyShapeLayer()
-        oval.frame = CGRect(x:30,y:30,width:80,height:80)
+        oval.frame = CGRect(x: screenCenter - 40,y:screenCenter - 40,width:80,height:80)
         oval.drawOval(lineWidth:1)
         self.view.layer.addSublayer(oval)
     }
     
-    /************** Touch Action ****************/
+    //画面がタッチされた時
     func hitLayer(touch:UITouch) -> CALayer{
-        var touchPoint:CGPoint = touch.location(in:self.view)
+        var touchPoint:CGPoint = touch.location(in:self.view)       //タッチされた座標取得
         touchPoint = self.view.layer.convert(touchPoint, to: self.view.layer.superlayer)
         return self.view.layer.hitTest(touchPoint)!
     }
+    
     func selectLayerFunc(layer:CALayer?) {
         if((layer == self.view.layer) || (layer == nil)){
             selectLayer = nil
@@ -143,31 +126,21 @@ class MapViewController: UIViewController, UITextFieldDelegate, UIGestureRecogni
         }
         selectLayer = layer
     }
-
     
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-
     // 画面にタッチで呼ばれるメソッド
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touch")
         
         // タッチイベントを取得
         let touchEvent = touches.first!
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         selectLayer = nil
-        //タッチを取得
-        let touch:UITouch = touches.first!
-        //タッチした場所にあるレイヤーを取得
-        let layer:CALayer = hitLayer(touch: touch)
-        //タッチされた座標を取得
-        let touchPoint:CGPoint = touch.location(in: self.view)
-        //最後にタッチされた場所に座標を入れて置く
-        touchLastPoint = touchPoint
-        //選択されたレイヤーをselectLayerにいれる
-        self.selectLayerFunc(layer:layer)
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+        let touch:UITouch = touches.first!        //タッチを取得
+        let layer:CALayer = hitLayer(touch: touch)        //タッチした場所にあるレイヤーを取得
+        let touchPoint:CGPoint = touch.location(in: self.view)        //タッチされた座標を取得
+        touchLastPoint = touchPoint        //最後にタッチされた場所に座標を入れて置く
+        self.selectLayerFunc(layer:layer)        //選択されたレイヤーをselectLayerにいれる
+        oldX = Double(touchPoint.x)
+        oldY = Double(touchPoint.y)
+        
         // ドラッグ前の座標, Swift 1.2 から
         let preDx = touchEvent.previousLocation(in: self.view).x
         let preDy = touchEvent.previousLocation(in: self.view).y
@@ -175,46 +148,40 @@ class MapViewController: UIViewController, UITextFieldDelegate, UIGestureRecogni
         //タッチした座標を取得
         preX = Double(preDx)
         preY = Double(preDy)
-
-        //円フレームの生成
-        path.addArc(withCenter: CGPoint(x: preX,y: preY), radius: 50, startAngle: start, endAngle: end, clockwise: true) // 円弧
-//        //レイヤー、パスの生成
-//        layer.fillColor = UIColor.orange.cgColor
-//        layer.path = path.cgPath
-//        path.close()
-//        playerView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//        //円フレームを追加
-//        self.view.layer.addSublayer(layer)
-
+        
+        let oval = MyShapeLayer()
+        oval.frame = CGRect(x:preX - 40,y:preY - 40,width:80,height:80)//新しい円フレームの設定
+        oval.drawOval(lineWidth:1)
         
         // Labelを生成
-        myLabel = UILabel(frame: CGRect(x: 0,y: 0,width: 100,height: 50))
-//        myLabel.backgroundColor = UIColor(red: 0.261, green: 0.737, blue: 0.561, alpha: 1.0)
+        myLabel = UILabel(frame: CGRect(x: preX,y: preY,width: 100,height: 50))
         myLabel.text = "new word"
         myLabel.textAlignment = NSTextAlignment.center
         myLabel.textColor = UIColor.white
-  
-//        // Labelを追加
-//        self.view.addSubview(myLabel)
-
+        
+        //レイヤーの内側がタッチされた時
+        if (selectLayer != nil){
+            //タイマー開始
+            timer = Timer.scheduledTimer(timeInterval: 0.01,target: self,selector: #selector(self.timerCounter),userInfo: nil,repeats: true)
+            //グローバル変数に格納
+            startTime = Date()
+            
+        }
+        
     }
-
-    
-    
     
     //　ドラッグ時に呼ばれるメソッド
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("drag")
-        
-        // タッチイベントを取得
-        let touchEvent = touches.first!
+        let touchEvent = touches.first!        // タッチイベントを取得
         
         let preDx = touchEvent.previousLocation(in: self.view).x
         let preDy = touchEvent.previousLocation(in: self.view).y
         //タッチした座標を取得
         preX = Double(preDx)
         preY = Double(preDy)
-        
+        moveX = oldX - preX
+        moveY = oldY - preY
+        print(moveX)
         
         let touchPoint:CGPoint = touchEvent.location(in:self.view)
         let touchOffsetPoint:CGPoint = CGPoint(x:touchPoint.x - touchLastPoint.x,
@@ -228,160 +195,86 @@ class MapViewController: UIViewController, UITextFieldDelegate, UIGestureRecogni
             //レイヤーを移動させる
             CATransaction.begin()
             CATransaction.setDisableActions(true)
-            selectLayer.position = CGPoint(x:px + touchOffsetPoint.x,y:py + touchOffsetPoint.y)
+            selectLayer.position = CGPoint(x:px + touchOffsetPoint.x,y:py + touchOffsetPoint.y) //選択されたレイヤーの位置
             selectLayer.borderWidth = 0.0
             selectLayer.borderColor = UIColor.green.cgColor
             CATransaction.commit()
+            
+            ovalBtn.center = CGPoint(x:px + touchOffsetPoint.x,y:py + touchOffsetPoint.y)
+            self.myLabel.center = CGPoint(x:px + touchOffsetPoint.x,y:py + touchOffsetPoint.y)
+            
         }
         
-//        //円フレームの生成
-//        path.addArc(withCenter: CGPoint(x: preDx,y: preDy), radius: 50, startAngle: start, endAngle: end, clockwise: true) // 円弧
-//        //円フレームを追加
-//        self.view.layer.addSublayer(layer)
-      
-//        //プレイヤーのドラッグするフレーム生成
-//        playerView.backgroundColor = UIColor.gray
-//        self.playerView.center = CGPoint(x: preDx, y: preDy)
-        self.myLabel.center = CGPoint(x: preDx, y: preDy)
-        
-//        //円フレームの生成
-//        path.addArc(withCenter: CGPoint(x: preX,y: preY), radius: 50, startAngle: start, endAngle: end, clockwise: true) // 円弧
-
-//        let path = UIBezierPath(ovalIn: CGRect(x: 0,y: 0,width: width,height: height))
-
-
-
-        //四角フレームを描画
-//        self.view.addSubview(playerView)
-//        //円フレームを追加
-//        self.view.layer.addSublayer(layer)
-
-        
-        
-//        // ドラッグしたx座標の移動距離
-//        var dx = newDx - preX
-//        
-//        // ドラッグしたy座標の移動距離
-//        let dy = newDy - preY
-        //print("\(newDx) - \(preDy) = \(dx)")
+//        self.myLabel.center = CGPoint(x: preDx, y: preDy)
     }
     
-    
-    // ドラッグ終了時に呼ばれるメソッド
+    // タッチ終了時に呼ばれるメソッド
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-
-        
-        
-        
-        
         
         // タッチイベントを取得
         let touchEvent = touches.first!
         //ドラッグ終了時の座標
         var newDx = Double(touchEvent.location(in: self.view).x)
         var newDy = Double(touchEvent.location(in: self.view).y)
-
+        
         // ドラッグしたx座標の移動距離
         var dx = newDx - preX
         
         // ドラッグしたy座標の移動距離
         let dy = newDy - preY
         
-        moveX = newDx - preX
-        moveY = newDy - preY
         
+        if timer != nil{
+            timer.invalidate()
+        }
+        frameAmount = 0
+        if newFrameLimit < 0.3 {
+            tap()
+        }
         
-        
-//        // UITextFieldを作成する.
-//        myTextField = UITextField(frame: CGRect(x: 0,y: 0,width: 200,height: 50))
-//        
-//        // 表示する文字を代入する.
-//        myTextField.text = "Input new word"
-//        
-//        // Delegateを設定する.
-//        myTextField.delegate = self
-//        
-//        // 枠を表示する.
-//        myTextField.borderStyle = UITextBorderStyle.roundedRect
-//        
-//        // UITextFieldの表示する位置を設定する.
-//        myTextField.layer.position = CGPoint(x:self.view.bounds.width/2,y:100);
-//        
-//        // Viewに追加する.
-//        self.view.addSubview(myTextField)
-//        
-//        
-//        /*
-//         UITextFieldが編集された直後に呼ばれるデリゲートメソッド.
-//         */
-//        func textFieldDidBeginEditing(textField: UITextField){
-//            print("textFieldDidBeginEditing:" + textField.text!)
-//        }
-//        
-//        /*
-//         UITextFieldが編集終了する直前に呼ばれるデリゲートメソッド.
-//         */
-//        func textFieldShouldEndEditing(textField: UITextField) -> Bool {
-//            print("textFieldShouldEndEditing:" + textField.text!)
-//            
-//            return true
-//        }
-//        
-//        /*
-//         改行ボタンが押された際に呼ばれるデリゲートメソッド.
-//         */
-//        func textFieldShouldReturn(textField: UITextField) -> Bool {
-//            textField.resignFirstResponder()
-//            
-//            return true
-//        }
-        
-        
-        
-
-        
-        
-        print(moveX)
-        print(moveY)
-        //print("\(newDx) - \(preX) = \(newDx - preX)")
-        print("endDrag")
     }
     
     
-
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-//    // AutoLayoutによる位置の設定がviewWillAppear以降で行われるので、その後にアニメーション処理を追記
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        
-//        // ①アニメーションするプロパティを指定し、オブジェクト作成
-//        let moveToCenter = CABasicAnimation(keyPath: "position.x")
-//        
-//        // ②アニメーションの初期値を指定する
-//        moveToCenter.fromValue = -view.bounds.size.width/2
-//        
-//        // ③アニメーションの終了時の値を指定する
-//        moveToCenter.toValue = view.bounds.size.width/2
-//        
-//        // ④アニメーションの時間を指定する
-//        moveToCenter.duration = 2.0
-//        
-//        // ⑤アニメーションをlayerに追加する
-//        self.view.layer.add(moveToCenter, forKey: nil)
-//    }
-//
+    func timerCounter() {
+        // タイマー開始からのインターバル時間
+        let currentTime = Date().timeIntervalSince(startTime)
+        
+        // fmod() 余りを計算
+        let minute = (Int)(fmod((currentTime/60), 60))
+        // currentTime/60 の余り
+        let second = (Int)(fmod(currentTime, 60))
+        // floor 切り捨て、小数点以下を取り出して *100
+        let msec = (Int)((currentTime - floor(currentTime))*100)
+        
+        // %02d： ２桁表示、0で埋める
+        let sMinute = String(format:"%02d", minute)
+        let sSecond = String(format:"%02d", second)
+        let sMsec = String(format:"%02d", msec)
+        
+        newFrameLimit = currentTime
+        
+        //1秒以上タッチされた時 &　ドラッグが大きいとき
+        if ( newFrameLimit >= 1.0 && newFrameLimit <= 100 && frameAmount < 1 && fabs(moveX) < 20 && fabs(moveY) < 20){
+            let oval = MyShapeLayer()
+            oval.frame = CGRect(x:preX - 40,y:preY - 40,width:80,height:80)//新しい円フレームの設定
+            oval.drawOval(lineWidth:1)
+            self.view.layer.addSublayer(oval)//円フレームの追加
+            selectLayer = oval
+            // Labelを追加
+            self.view.addSubview(myLabel)
+            frameAmount += 1
+            
+        }
+        
+        
+    }
     
     
     
-    
-
-   
 }
